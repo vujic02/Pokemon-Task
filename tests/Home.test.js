fc5f675816b917278d14937d4fef5076a5291452
vujic02 <vujicn02@gmail.com>
2021-09-13 14:03:19 +0200

    removed extra test data

--- tests/Home.test.js
+++ tests/Home.test.js
@@ -10,7 +10,7 @@ describe("Home page", () => {
   beforeEach(() => {
     pokemons = {
       results: [
-        {name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/', abilities: [{ability: "overgrow"}, {ability: "chlorophyll"}]},
+        {name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/'},
         {name: 'ivysaur', url: 'https://pokeapi.co/api/v2/pokemon/2/'},
         {name: 'venusaur', url: 'https://pokeapi.co/api/v2/pokemon/3/'},
         {name: 'charmander', url: 'https://pokeapi.co/api/v2/pokemon/4/'},
